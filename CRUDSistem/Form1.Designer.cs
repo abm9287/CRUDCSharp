3d18b4edd3d4acbfdabb279e2e72569a185e7024
venom <abm.chamba@gmail.com>
2020-07-26 18:47:00 -0500

    Buttons and labels.

---
+++ CRUDSistem/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CRUDSistem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNombres = new System.Windows.Forms.Label();
+            this.labelApellidos = new System.Windows.Forms.Label();
+            this.textBoxNombres = new System.Windows.Forms.TextBox();
+            this.textBoxApellidos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelNombres
+            // 
+            this.labelNombres.AutoSize = true;
+            this.labelNombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombres.Location = new System.Drawing.Point(66, 100);
+            this.labelNombres.Name = "labelNombres";
+            this.labelNombres.Size = new System.Drawing.Size(80, 20);
+            this.labelNombres.TabIndex = 0;
+            this.labelNombres.Text = "Nombres";
+            // 
+            // labelApellidos
+            // 
+            this.labelApellidos.AutoSize = true;
+            this.labelApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelApellidos.Location = new System.Drawing.Point(67, 146);
+            this.labelApellidos.Name = "labelApellidos";
+            this.labelApellidos.Size = new System.Drawing.Size(82, 20);
+            this.labelApellidos.TabIndex = 1;
+            this.labelApellidos.Text = "Apellidos";
+            // 
+            // textBoxNombres
+            // 
+            this.textBoxNombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNombres.Location = new System.Drawing.Point(167, 100);
+            this.textBoxNombres.Name = "textBoxNombres";
+            this.textBoxNombres.Size = new System.Drawing.Size(313, 26);
+            this.textBoxNombres.TabIndex = 2;
+            this.textBoxNombres.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxApellidos
+            // 
+            this.textBoxApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxApellidos.Location = new System.Drawing.Point(167, 146);
+            this.textBoxApellidos.Name = "textBoxApellidos";
+            this.textBoxApellidos.Size = new System.Drawing.Size(313, 26);
+            this.textBoxApellidos.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxApellidos);
+            this.Controls.Add(this.textBoxNombres);
+            this.Controls.Add(this.labelApellidos);
+            this.Controls.Add(this.labelNombres);
+            this.Name = "Form1";
+            this.Text = "DatosPersona";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNombres;
+        private System.Windows.Forms.Label labelApellidos;
+        private System.Windows.Forms.TextBox textBoxNombres;
+        private System.Windows.Forms.TextBox textBoxApellidos;
+    }
+}
+
